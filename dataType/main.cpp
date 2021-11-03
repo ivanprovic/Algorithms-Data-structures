25368400344c69160892d3031062e6600cb04aa6
Ivan Provic <ivan.pik82@gmail.com>
2021-11-03 14:54:28 +0100

    add work on obj

---
+++ dataType/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "bool:\t\t" << sizeof(bool) << " bytes\n";
+    std::cout << "char:\t\t" << sizeof(char) << " bytes\n";
+    std::cout << "wchar_t:\t" << sizeof(wchar_t) << " bytes\n";
+
+
+    return 0;
+}
