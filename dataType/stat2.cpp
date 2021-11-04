f5caba2c90d82a96b48fdd846a63b6fd1a5eeb03
Ivan Provic <ivan.pik82@gmail.com>
2021-11-04 22:21:40 +0100

    add work on obj

---
+++ dataType/stat2.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "Enter an integer: ";
+    int x{};
+    std::cin >> x;
+
+    if (x == 0)
+        std::cout << "The value is zero \n";
+    
+    if (x != 0)
+        std::cout << "The value is non-zero \n";
+
+    return 0;
+}
