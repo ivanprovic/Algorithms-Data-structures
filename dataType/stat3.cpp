aa14f6cc623da32a252fe33e70e99c873d5c5874
Ivan Provic <ivan.pik82@gmail.com>
2021-11-04 22:38:49 +0100

    add work on obj

---
+++ dataType/stat3.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+// returns true if x and y are equal, false otherwise
+bool isEqual(int x, int y)
+{
+    return (x == y ); // operator== returns true if x == y, false otherwise
+}
+
+int main()
+{
+    std:: cout << "Enter an integer: ";
+    int x{};
+    std:: cin >> x;
+
+    std::cout << "Enter another integer: ";
+    int y{};
+    std:: cin >> y;
+
+    if(isEqual(x, y))
+        std::cout << x << " and " << y << "are equal\n";
+    else
+        std::cout << x << " and " << y << "are not equal\n";
+
+    return 0;
+}
+
