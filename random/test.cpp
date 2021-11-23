7851bc81b872b09043662079ccdc35228d000926
Ivan Provic <ivan.pik82@gmail.com>
2021-11-23 14:39:12 +0100

    add work on array

---
+++ random/test.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    double batteryLifeInHours[3]{}; // allocate 3 doubles
+    batteryLifeInHours[0] = 2.0;
+    batteryLifeInHours[1] = 3.0;
+    batteryLifeInHours[2] = 4.3;
+
+    std::cout << "The average battery life is " << (batteryLifeInHours[0] + batteryLifeInHours[1]
+        + batteryLifeInHours[2]) / 3.0 << " hour(s)\n";
+
+    return 0;
+}
