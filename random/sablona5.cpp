874013b998101a45e51bc1f53c23759572624e6c
Ivan Provic <ivan.pik82@gmail.com>
2021-12-01 20:54:15 +0100

    vectors

--- random/sablona5.cpp
+++ random/sablona5.cpp
@@ -1,11 +1,16 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
+// first take on vectors
 int main()
 {
-  int myarray[] = {2,4,6,8};
-  for (int i=0; i<4; i++){
-    cout <<myarray[i] <<endl;
-}
+  vector <int> intvector;
+  intvector.reserve(50);
+
+  for (int i = 0; i<50; i++){
+    intvector.push_back(i*i);
+    cout << intvector[i] << '\n';
+  }
   return 0;
 }
