3aa8f0ea0e9ebcf7a5b8c88d4530d5b526deeaa0
Ivan Provic <ivan.pik82@gmail.com>
2021-12-04 22:19:24 +0100

    another hash table

---
+++ hashtable/test2.cpp
@@ -0,0 +1,15 @@
+// hash table in C++
+#include <iostream>
+#include <list>
+using namespace std;
+
+class HashTable
+{
+    int capacity;
+    list<int> *table;
+};
+
+int main()
+{
+    return 0;
+}
