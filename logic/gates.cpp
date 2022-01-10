2c3a84bd06d7745e65d20bfddba86b1bc99f4df3
Ivan Provic <ivan.pik82@gmail.com>
2022-01-10 22:46:39 +0100

    created base class

--- logic/gates.cpp
+++ logic/gates.cpp
@@ -1,7 +1,34 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
+//creates a class with logic gate
+//that returns label and boolean val
 
+class LogicGate {
+    public:
+        LogicGate(string n) {
+            label = n;
+        }
+        string getLabel(){
+            return label;
+        }
+        bool getOutput(){
+            output = performGateLogic();
+            return output;
+        }
+        virtual bool performGateLogic(){
+            cout << "ERROR! performGateLogic BASE" << '\n';
+            return false; 
+        }
+
+        virtual void setNextPin(bool source) {
+            cout << "ERROR! setNextPin BASE" << '\n';
+        }
+    private:
+        string label;
+        bool output;
+};
 
 int main()
 {
