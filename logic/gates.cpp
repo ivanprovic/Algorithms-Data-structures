0a5e2ec9506848b8db6ab17e82ca78ea4cf1b30a
Ivan Provic <ivan.pik82@gmail.com>
2022-01-12 18:31:21 +0100

    set labels and connections final commit

--- logic/gates.cpp
+++ logic/gates.cpp
@@ -162,5 +162,33 @@ class Connector {
 
 int main()
 {
+    char stopme;
+
+    //setting labels
+    AndGate gand1(" AND1 ");
+    AndGate gand2(" AND2 ");
+    OrGate gor3(" OR ");
+    NotGate gnot4(" NOT ");
+
+    //The inputs can be changed here
+    gand1.setNextPin(0);
+    gand1.setNextPin(0);
+    gand2.setNextPin(0);
+    gand2.setNextPin(0);
+
+    //making connections
+    Connector c1(&gand1, &gor3);    
+    Connector c2(&gand2, &gor3);
+    Connector c3(&gor3, &gnot4);
+
+    cout << gnot4.getLabel() << "(";
+    cout << "(" << gand1.getPinA() << gand1.getLabel() << gand1.getPinB() << ")";
+    cout << gor3.getLabel();
+    cout << "(" << gand2.getPinA() << gand2.getLabel() << gand2.getPinB() << ")";
+    cout << ") results in " << gnot4.getOutput() << endl;
+
+    cin >> stopme; //holds open window under some conditions.    
+
+
     return 0;
 }
