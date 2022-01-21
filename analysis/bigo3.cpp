b6169c5a01729efde6688c911df012bb6ef42052
Ivan Provic <ivan.pik82@gmail.com>
2022-01-21 16:43:35 +0100

    started 0s and 1s segregation bigO

---
+++ analysis/bigo3.cpp
@@ -0,0 +1,18 @@
+/* c++ code to segregate 0s and 1s in an array
+You are given an array of 0s and 1s in random order. Segregate 0s on left side and 1s on right
+side of the array [Basically you have to sort the array]. Traverse array only once. 
+time complexity 0(n)
+*/
+
+#include <iostream>
+using namespace std;
+
+void segregate0and1(int arr[], int n)
+{
+  //finish 
+}
+
+int main()
+{
+  return 0;
+}
