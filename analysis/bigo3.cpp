4ff9cd45c911caaf4d25615ba1c48300ec76374c
Ivan Provic <ivan.pik82@gmail.com>
2022-01-24 18:25:50 +0100

    finished segregate0and1 algorithm

--- analysis/bigo3.cpp
+++ analysis/bigo3.cpp
@@ -9,17 +9,37 @@ using namespace std;
 
 void segregate0and1(int arr[], int n)
 {
-  //finish 
+  int count = 0; //counts the no of zeros in array
+
+  for (int i = 0; i < n; i++) {
+    if (arr [i] == 0)
+      count++;
+  }
+
+  //loop fills the arr with 0 until count
+  for (int i = 0; i < count; i++)
+    arr[i] = 0;
+
+  //loop fills remaining arr space with 1
+  for (int i = count; i < n; i++)
+    arr[i] = 1;
 }
 
 void print(int arr[], int n)
 {
-  //print segregated array
+  cout << "Array after segregation is: ";
+
+  for (int i = 0; i < n; i++)
+    cout << arr[i] << " ";
 }
 
 int main()
 {
   int arr[] = {0, 1, 0, 1, 1, 1};
   int n = sizeof(arr) / sizeof(arr[0]);
+
+  segregate0and1(arr, n);
+  print (arr, n);
+ 
   return 0;
 }
