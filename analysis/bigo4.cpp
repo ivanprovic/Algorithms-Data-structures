be578091947be564ea8cfd681bc33810430a39cf
Ivan Provic <ivan.pik82@gmail.com>
2022-01-29 11:12:02 +0100

    finished algorithm

--- analysis/bigo4.cpp
+++ analysis/bigo4.cpp
@@ -12,6 +12,20 @@ void segregate0and1(int arr[], int size)
       //increment left index while we see 0 at left
       while (arr[left] == 0 && left < right)
 	left++;
+
+      //decrement right index while we see 1 at right
+      while (arr[right] == 1 && left < right)
+	right--;
+
+      /* if left is smaller than right then there is a 1 at left and a 0
+	 at right. Exchange arr[left] and arr [right] */
+      if (left < right)
+	{
+	  arr[left] = 0;
+	  arr[right] = 1;
+	  left++;
+	  right--;
+	}
     }
 }
 
@@ -19,6 +33,11 @@ int main()
 {
   int arr[] = {0, 1, 0, 1, 1, 1};
   int i, arr_size = sizeof(arr)/sizeof(arr[0]);
-  
+
+  segregate0and1(arr, arr_size);
+
+  cout << "Array after segregation ";
+  for (i = 0; i < 6; i++)
+     cout << arr[i] << " ";
   return 0;
 }
