6000792fd9ddc54f53f3366e6934e04123f84751
Ivan Provic <ivan.pik82@gmail.com>
2022-01-30 21:17:22 +0100

    created template for bigO

---
+++ analysis/bigo5.cpp
@@ -0,0 +1,16 @@
+//C++ program to sort a binary in one pass
+#include <iostream>
+using namespace std;
+
+/*Function to put all 0s on left and all 1s on right*/
+void segregate0and1(int arr[], int size)
+{
+    int type0 = 0;
+    int type1 = size - 1;
+}
+
+int main()
+{
+    int arr[] = {0, 1, 0, 1, 1, 1};
+    int i, arr_size = sizeof(arr) / sizeof(arr[0]);
+}
