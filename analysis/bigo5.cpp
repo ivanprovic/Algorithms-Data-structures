7c18a1020d97eccba9cd3d78dc7e7896d92e92cc
Ivan Provic <ivan.pik82@gmail.com>
2022-01-30 22:12:33 +0100

    finished

--- analysis/bigo5.cpp
+++ analysis/bigo5.cpp
@@ -1,4 +1,4 @@
-//C++ program to sort a binary in one pass
+//C++ program to sort a binary in one pass using
 #include <iostream>
 using namespace std;
 
@@ -7,10 +7,31 @@ void segregate0and1(int arr[], int size)
 {
     int type0 = 0;
     int type1 = size - 1;
+
+    while(type0 < type1)
+    {
+        if(arr[type0] == 1)
+        {
+            swap(arr[type0],
+                arr[type1]);
+            type1--;
+        }
+        else
+        type0++;
+    }
 }
 
 int main()
 {
     int arr[] = {0, 1, 0, 1, 1, 1};
     int i, arr_size = sizeof(arr) / sizeof(arr[0]);
+
+    segregate0and1(arr, arr_size);
+
+    cout << "Array after segregation is ";
+    for (i = 0; i < arr_size; i++)
+        cout << arr[i] << " ";
+
+        return 0;
+
 }
